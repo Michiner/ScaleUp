583d868780582541bd1fe81f1367697df77c0f28
Michiner <amikami333@gmail.com>
2022-04-20 14:03:23 +0300

    create board with js(and not html)

--- CessBoard/chassBoard.js
+++ CessBoard/chassBoard.js
@@ -8,7 +8,8 @@ const WHITETYPE = 'white';
 const BLACkTYPE = 'black';
 let row;
 let selectedCell;
-let peices = [];
+let pieces = [];
+
 
 
 class Piece {
@@ -18,15 +19,6 @@ class Piece {
         this.type = type;
         this.player = player;
     }
-}
-peices = getInitialBoard();
-
-
-function getInitialBoard() {
-    let result = [];
-    result.push(new Piece(0, 0, "rook", WHITETYPE))
-    result.push(new Piece(0, 1, "knight", WHITETYPE))
-    return result;
 }
 const table = document.createElement('table');  // make the table
 document.body.append(table);
@@ -42,61 +34,67 @@ for (let i = 0; i < 8; i++) {                 // make the rows
         row.append(cell);
         cell.id = "cell-" + i.toString() + "_" + j.toString();
         
-    if ((j + i) % 2 == 0) {    // make the peaces color
-        cell.classList.add('white');
-    } else {
-    cell.classList.add('black');
-    }
-    
-    
-    function addImage(cell, type, name) {  // make the peaces photos
-        const image = document.createElement('img');
-        image.src = 'Photos/' + type + '/' + name + '.png';
-        cell.appendChild(image);
-    }
-    
-    
-    function addImageByIndex(cell, type, index) { // orginaiz the photos per peace
-        if (index === 0 || index === 7) {
-            addImage(cell, type, 'rook');
-        } else if (index === 1 || index === 6) {
-            addImage(cell, type, 'knight');
-        } else if (index === 2 || index === 5) {
-            addImage(cell, type, 'bishop');
-        } else if (index === 3) {
-            addImage(cell, type, 'king');
-        } else if (index === 4) {
-            addImage(cell, type, 'queen');
+        if ((j + i) % 2 == 0) {    // make the peaces color
+            cell.classList.add('white');
+        } else {
+            cell.classList.add('black');
         }
+        function OnCellClick(event) { 
+            console.log(event.currentTarget);
+            if (selectedCell !== undefined){
+                selectedCell.classList.remove('selected');  
+            }
+            selectedCell = event.currentTarget;
+            selectedCell.classList.add('selected'); 
+        } 
+        cell.onclick = OnCellClick;
+} }       // עד פה יצירת הטבלה והלחיצה
+        function addImage(cell, type, name) {  // make the peaces photos
+            const image = document.createElement('img');
+            image.src = 'Photos/' + type + '/' + name + '.png';
+            cell.appendChild(image);
+            
+        }
+        pieces = getInitialBoard();
         
-    }
+        
+        function getInitialBoard() {
+            let result = [];
+            result.push(new Piece(0, 0, "rook", WHITETYPE));
+            result.push(new Piece(0, 1, "knight", WHITETYPE));
+            result.push(new Piece(0, 2, "BISHOP", WHITETYPE));
+            result.push(new Piece(0, 3, "KING", WHITETYPE));
+            result.push(new Piece(0, 4, "QUEEN", WHITETYPE));
+            result.push(new Piece(0, 5, "BISHOP", WHITETYPE));
+            result.push(new Piece(0, 6, "KNIGHT", WHITETYPE));
+            result.push(new Piece(0, 7, "ROOK", WHITETYPE));
 
 
-    if (i === 0) {
-        addImageByIndex(cell, BLACkTYPE, j);
-    } else if (i === 1) {
-        addImage(cell, BLACkTYPE, 'pawn');
-    } else if (i === 6) {
-        addImage(cell, WHITETYPE, 'pawn');
-    } else if (i === 7) {
-        addImageByIndex(cell, WHITETYPE, j);
-    }
-    
-    for (let piece of peices) {
-        // addImage(table.rows[piece.row].cells[piece.col], piece.player, piece.type);  עידו, פה נוצרת לי הבעיה לדעתי ולא מצליח להבין למה
-    console.log( 'pieces', peices);
-    }
-    
-    
-    function OnCellClick(event) { 
-        console.log(event.currentTarget);
-        if (selectedCell !== undefined){
-            selectedCell.classList.remove('selected');  
+            result.push(new Piece(7, 0, "rook", BLACkTYPE));
+            result.push(new Piece(7, 1, "knight", BLACkTYPE));
+            result.push(new Piece(7, 2, "BISHOP", BLACkTYPE));
+            result.push(new Piece(7, 3, "KING", BLACkTYPE));
+            result.push(new Piece(7, 4, "QUEEN", BLACkTYPE));
+            result.push(new Piece(7, 5, "BISHOP", BLACkTYPE));
+            result.push(new Piece(7, 6, "KNIGHT", BLACkTYPE));
+            result.push(new Piece(7, 7, "ROOK", BLACkTYPE));
+
+            for (let i = 0; i < 8; i++){
+                result.push(new Piece(1, i, "pawn", WHITETYPE));
+                result.push(new Piece(6, i, "pawn", BLACkTYPE));
+
+            }
+
+
+            return result;
         }
-        selectedCell = event.currentTarget;
-        selectedCell.classList.add('selected'); 
-    } 
-    cell.onclick = OnCellClick;
+    
+
+    
+    for (let piece of pieces) {
+        addImage(table.rows[piece.row].cells[piece.col], piece.player, piece.type);  
+    console.log( 'pieces', pieces);
+    }
     
     
-}}
+    
